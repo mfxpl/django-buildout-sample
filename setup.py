2f830a67b4f1950a46ddf1e940b30952da17feba
m.frackowiak <michal.f@mail.com>
2016-12-18 21:12:45 +0100

    commit

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-from setuptools import setup, find_packages
-
+from distutils.core import setup
 setup(
-    name = "django-shorturls",
-    version = "1.0",
-    url = 'https://github.com/mfxpl/django-buildout-sample.git',
-    license = 'GNU',
-    description = "A short URL handler for Django apps.",
-    author = 'Michał Frąckowiak',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
+  name = 'django-buildout-sample',
+  packages = ['django-buildout-sample'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Michał Frąckkowiak',
+  author_email = 'mfxovh@gmail.com',
+  url = 'https://github.com/mfxpl/django-buildout-sample', # use the URL to the github repo
+  download_url ='https://github.com/mfxpl/django-buildout-sample/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
 )
+
+
+# from setuptools import setup, find_packages
+#
+# setup(
+#     name = "django-shorturls",
+#     version = "1.0",
+#     url = 'https://github.com/mfxpl/django-buildout-sample.git',
+#     license = 'GNU',
+#     description = "A short URL handler for Django apps.",
+#     author = 'Michał Frąckowiak',
+#     packages = find_packages('src'),
+#     package_dir = {'': 'src'},
+#     install_requires = ['setuptools'],
+# )
